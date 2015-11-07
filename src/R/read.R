ea06bc92335b462808fb5644ffb926e653868a89
Alex Perrone <alexander.perrone@jhuapl.edu>
2015-11-07 11:23:01 -0700

    added basic comparisons in R (not shiny yet).

--- src/R/read.R
+++ src/R/read.R
@@ -1,5 +1,6 @@
 library("data.table")
 library("lubridate")
+library("magrittr")
 
 # Read in the data. 
 dat <- fread("data/dqcdm-temporal-summary/dqcdm_temporal_summary_subset_2.txt")
@@ -10,7 +11,7 @@ dat[ , time_period := paste0(time_period, "01")]
 dat[ , time_period := ymd(time_period)]
 
 
-# Exploratory data analysis. 
+# Exploratory data analysis.  
 ggplot(subset(dat, concept_id==312664), aes(x=time_period , y=prevalence)) + 
   geom_line(aes(group=1)) + 
   geom_point() + 
