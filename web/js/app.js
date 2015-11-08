51889efb988ac5a57fdefd6eb914870b26d83c4d
Anthony Sena <asena5@its.jnj.com>
2015-11-08 00:48:49 -0500

    Web improvements

--- web/js/app.js
+++ web/js/app.js
@@ -21,42 +21,49 @@ define([
 		this.services = [
 			{
 				name: 'CCAE',
+                sourceName: 'Truven CCAE',
 				url: 'js/json/ccae/',
 				dialect: ko.observable('loading'),
 				version: ko.observable('loading')
 			},
 			{
 				name: 'OPTUM',
+                sourceName: 'Optum',
 				url: 'js/json/optum/',
 				dialect: ko.observable('loading'),
 				version: ko.observable('loading')
 			},
 			{
 				name: 'MDCR',
+                sourceName: 'Truven MDCR',
 				url: 'js/json/mdcr/',
 				dialect: ko.observable('loading'),
 				version: ko.observable('loading')
 			},
 			{
 				name: 'MDCD',
+                sourceName: 'Truven MDCD',
 				url: 'js/json/mdcd/',
 				dialect: ko.observable('loading'),
 				version: ko.observable('loading')
 			},
 			{
 				name: 'CPRD',
+                sourceName: 'CPRD',
 				url: 'js/json/cprd/',
 				dialect: ko.observable('loading'),
 				version: ko.observable('loading')
 			},
 			{
 				name: 'JMDC',
+                sourceName: 'JMDC',
 				url: 'js/json/jmdc/',
 				dialect: ko.observable('loading'),
 				version: ko.observable('loading')
 			},
 			{
 				name: 'PREMIER',
+                sourceName: 'premier',
 				url: 'js/json/premier/',
 				dialect: ko.observable('loading'),
 				version: ko.observable('loading')
@@ -68,30 +75,29 @@ define([
 		this.currentSummaryStats = ko.observable();
         this.currentSearch = ko.observable();
         this.currentConceptId = ko.observable();
+        this.currentConceptName = ko.observable();
 
         self.initComplete = function () {
             self.router.init('/');
         }
 
 		this.getSummaryStats = function() {
-				// Retrieve the summary stats from the static JSON files
-				$.ajax({
-					url: self.currentServiceUrl() + 'summary.json',
-					success: function (results) {
-						self.currentSummaryStats(results);
-					},
-					error: function (xhr, message) {
-						alert('Could not find ' + self.currentServiceUrl() + 'summary.json');
-					}
-				});        	
-
+            // Retrieve the summary stats from the static JSON files
+            $.ajax({
+                url: self.currentServiceUrl() + 'summary.json',
+                success: function (results) {
+                    self.currentSummaryStats(results);
+                },
+                error: function (xhr, message) {
+                }
+            });
 		}
 		
         this.currentSummaryStats(this.getSummaryStats());
 
         this.currentService.subscribe(function (newValue) {
         	if (newValue != undefined) {        		
-        		this.getSummaryStats();
+        		self.getSummaryStats();
         	}
         });
         
